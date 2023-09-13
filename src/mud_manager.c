adef899a1472ddd4a537ea1a3fd64e676dc1d0bd
Luca Mannella <luca.mannella@polito.it>
2023-09-13 18:00:47 +0200

    modifying DENY to DROP

--- src/mud_manager.c
+++ src/mud_manager.c
@@ -218,7 +218,7 @@ int executeMudWithDhcpContext(DhcpEvent *dhcpEvent)
 		    }
 
 		    // Install default rule to block all traffic from this IP address unless allowed in the MUD file
-		    // ORDER MATTERS - this rule needs to be installed after all of the individual allow/deny rules
+		    // ORDER MATTERS - this rule needs to be installed after all of the individual allow/drop rules
 			actionResult = installFirewallIPRule(dhcpEvent->ipAddress, 		/* srcIp */
 													"any", 					/* destIp */
 													"any",		 			/* destPort */
@@ -226,7 +226,7 @@ int executeMudWithDhcpContext(DhcpEvent *dhcpEvent)
 													WAN_DEVICE_NAME,		/* destDevice - lan or wan */
 													"all", 					/* protocol - tcp/udp */
 													"REJECT-ALL", 			/* the name of the rule -- TODO: Better rule names by device name*/
-													"DENY",					/* ACCEPT or DENY or REJECT */
+													"DROP",					/* ACCEPT or DROP or REJECT */
 													"all",
 													dhcpEvent->hostName		/* hostname of the new device */ );
 			if (actionResult) {
